b943d61ae4b094f5f4ab52ee17b4b5fdcaa188c4
maksdudin <maks-dudin@mail.ru>
2023-05-27 23:33:06 +0200

    Initial commit да просто проба пера

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,15 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( Calculator.add(2,3));
+        System.out.println( Calculator.multiplay(2,3));
+        System.out.println( Calculator.divide(2,2));
+    }
+}
